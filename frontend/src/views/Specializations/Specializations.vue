9a5d0d1d5df3bd2cde87b864e3cc735cc7c8b2ad
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2024-03-25 15:58:17 +0100

    working good

---
+++ frontend/src/views/Specializations/Specializations.vue
@@ -0,0 +1,17 @@
+<template>
+    <Navbar/>
+    <div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Navbar from '../Navbar/Navbar.vue';
+
+export default defineComponent({
+    components: {
+        Navbar
+    },
+    
+})
+</script>
