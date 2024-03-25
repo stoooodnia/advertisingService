30a0473b8735cfed6c5976420236aff7e87a4cb9
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2024-03-25 10:49:09 +0100

    tailwind colors config + css body

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "my-purple": "#636AAF",
+      "my-red": "#ff0000",
+      "my-black": "#131314",
+      "my-gray": "#565656",
+      "my-light-gray": "#DCDCDC",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
