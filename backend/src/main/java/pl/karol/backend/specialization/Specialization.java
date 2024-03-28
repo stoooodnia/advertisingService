bc3a078e3328d7671c832784c21adc9a04f05305
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2024-03-28 18:47:13 +0100

    specialization validation annotations

--- backend/src/main/java/pl/karol/backend/specialization/Specialization.java
+++ backend/src/main/java/pl/karol/backend/specialization/Specialization.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,5 +21,7 @@ public class Specialization {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @NotBlank(message = "Label is mandatory")
+    @Size (min = 3,max = 20, message = "Label must be between 3 and 20 characters long")
     private String label;
 }
