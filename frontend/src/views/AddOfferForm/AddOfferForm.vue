38e996a41675451fe1e3f3855ea91f8e005af2d0
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2024-03-29 14:43:43 +0100

    basic form validation

--- frontend/src/views/AddOfferForm/AddOfferForm.vue
+++ frontend/src/views/AddOfferForm/AddOfferForm.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-if="show" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-        <form @submit.prevent class="flex flex-col gap-8 bg-white rounded-lg w-[574px] h-[632px] p-6">
+        <form @submit.prevent="submit" class="flex flex-col gap-8 bg-white rounded-lg w-[574px] h-[632px] p-6">
             <header class="flex justify-between">
                 <h5 class="text-2xl font-semibold">Dodaj ogłoszenie</h5>
                 <Button class="p-[10px] rounded-xl" size="iconOnly" icon="fa-solid fa-x" color="black" :action="close"/>
@@ -8,24 +8,24 @@
             <div class="flex flex-col gap-4">
                 <div class="w-full flex flex-col gap-2">
                     <label class="text-[#565656] text-[14px] font-semibold" for="firstname">Imię</label>
-                    <input v-model="form.firstname" id="firstname" type="text" placeholder="Wpisz swoję imię" class="h-[44px] border border-my-light-gray rounded-xl p-3  placeholder-my-gray text-[14px]">
+                    <input required v-model="form.firstname" id="firstname" type="text" placeholder="Wpisz swoję imię" class="h-[44px] border border-my-light-gray rounded-xl p-3  placeholder-my-gray text-[14px]">
                 </div>
                 <div class="w-full flex flex-col gap-2">
                     <label class="text-[#565656] text-[14px] font-semibold" for="lastname">Nazwisko</label>
-                    <input v-model="form.lastname" id="lastname" type="text" placeholder="Wpisz swoję nazwisko" class="h-[44px] border border-my-light-gray rounded-xl p-3  placeholder-my-gray text-[14px]">
+                    <input required v-model="form.lastname" id="lastname" type="text" placeholder="Wpisz swoję nazwisko" class="h-[44px] border border-my-light-gray rounded-xl p-3  placeholder-my-gray text-[14px]">
                 </div>
                 <div class="w-full flex flex-col gap-2">
                     <label class="text-[#565656] text-[14px] font-semibold " for="specOption">Specjalizacja</label>
-                    <Select v-model="form.specOption" class="w-full" id="specOption" />
+                    <Select required v-model="form.specOption" class="w-full" id="specOption" />
                 </div>
             </div>
             <div class="flex flex-col gap-4">
                 <label class="text-[#565656] text-[14px] font-semibold" for="content">Treść ogłoszenia</label>
-                <textarea v-model="form.content" id="content" placeholder="Wpisz treść ogłoszenia" maxlength="50" class="border border-my-light-gray rounded-xl p-3 h-[120px] resize-none placeholder-my-gray text-[14px]"></textarea>
+                <textarea required v-model="form.content" id="content" placeholder="Wpisz treść ogłoszenia" maxlength="50" class="border border-my-light-gray rounded-xl p-3 h-[120px] resize-none placeholder-my-gray text-[14px]"></textarea>
             </div>
             <div class="flex justify-end items-end gap-2 h-full">
                     <Button class="rounded-lg px-[16px]" :action="close" color="black" size="labelOnly" label="Anuluj"/>
-                    <Button class="rounded-lg px-[30px]" :action="submit" color="gray" size="labelOnly" label="Zapisz"/>
+                    <Button type=submit class="rounded-lg px-[30px]" :action="()=>{}" color="gray" size="labelOnly" label="Zapisz"/>
             </div>
         </form>
     </div>
@@ -63,7 +63,13 @@ export default defineComponent({
         },
         submit() {
           console.log(JSON.stringify(this.form))
-          
+          this.form = {
+            firstname: "",
+            lastname: "",
+            content: "",
+            specOption: "",
+          }
+          this.close()
         },
     }
 })
