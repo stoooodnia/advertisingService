74f845a7857cf5ea6dd6fe216c57a1f90409b303
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2024-04-22 21:01:56 +0200

    disabled authentication

--- backend/src/main/java/pl/karol/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/pl/karol/backend/config/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package pl.karol.backend.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -42,10 +42,11 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf((csrf) -> csrf.disable())
+                .httpBasic((basic) -> basic.securityContextRepository(new HttpSessionSecurityContextRepository()))
             .authorizeHttpRequests(
                     (authorizeRequests) -> authorizeRequests
-                            .requestMatchers(BLACK_LIST_URLS)
-                            .authenticated()
+//                            .requestMatchers(BLACK_LIST_URLS)
+//                            .authenticated()
                             .anyRequest()
                             .permitAll()
             )
